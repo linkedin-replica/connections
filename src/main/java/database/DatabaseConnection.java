e1b4ca47b69a93c75a0e58e365f1743837af51b7
Esraa <esraasalah171995@gmail.com>
2018-02-23 22:37:03 +0200

    Initial skeleton

---
+++ src/main/java/database/DatabaseConnection.java
@@ -0,0 +1,15 @@
+package main.java.database;
+
+/**
+ * A singleton class carrying a database instance
+ */
+public class DatabaseConnection {
+	
+	private DatabaseConnection(){
+		
+	}
+	
+	public static DatabaseConnection getInstance(){
+		if()
+	}
+}
