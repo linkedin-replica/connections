c1c0dc11ac622fca9214870968e82867b0f565c5
Esraa <esraasalah171995@gmail.com>
2018-02-23 23:49:48 +0200

    DatabaseHandler Interface Done

---
+++ src/main/java/databaseHandlers/DatabaseHandler.java
@@ -0,0 +1,24 @@
+package main.java.databaseHandlers;
+
+public interface DatabaseHandler {
+	/**
+	 * Block a user 
+	 */
+	public void blockUser(String name);
+	
+	/**
+	 * Add user a friend 
+	 */
+	public void addFriend(String name);
+	
+	/**
+	 * Accept friend request 
+	 */
+	public void accpetFriendRequest(String name);
+	
+	/**
+	 * Unfriend a user 
+	 */
+	public void unfriendUser(String name);
+	
+}
