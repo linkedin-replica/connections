4e70bbb0ef5071ad6942c2aec0bdb6752c2df659
ahmed-anwar <ahmedanwarm@gmail.com>
2018-03-23 08:07:31 +0200

    add user and friends list models

---
+++ src/main/java/com/linkedin/replica/connections/models/User.java
@@ -0,0 +1,74 @@
+package com.linkedin.replica.connections.models;
+
+import java.util.ArrayList;
+
+public  class User {
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String headline;
+    private String imageUrl;
+    private ArrayList<FriendsList> friendsList;
+
+
+    public User() {};
+
+    public User(String userId, String firstName, String lastName, String headline,
+                String imageUrl, ArrayList<FriendsList> friendsList) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.headline = headline;
+        this.imageUrl = imageUrl;
+        this.friendsList = friendsList;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getHeadline() {
+        return headline;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public ArrayList<FriendsList> getFriendsList() {
+        return friendsList;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setHeadline(String headline) {
+        this.headline = headline;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public void setFriendsList(ArrayList<FriendsList> friendsList) {
+        this.friendsList = friendsList;
+    }
+
+}
