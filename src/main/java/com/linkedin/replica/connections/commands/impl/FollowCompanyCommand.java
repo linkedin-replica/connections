3fbaa15723c6c7103c52717c75e59c0e6ea7855b
Nabila63Ahmed <nabila.ahmed63@gmail.com>
2018-04-19 23:32:56 +0200

    :sparkles: Un/Follow companies commands

--- src/main/java/com/linkedin/replica/connections/commands/impl/FollowCompanyCommand.java
+++ src/main/java/com/linkedin/replica/connections/commands/impl/FollowCompanyCommand.java
@@ -1,4 +1,26 @@
 package com.linkedin.replica.connections.commands.impl;
 
-public class FollowCompanyCommand {
+import com.linkedin.replica.connections.commands.Command;
+import com.linkedin.replica.connections.database.handlers.FollowingCompanies;
+import com.linkedin.replica.connections.database.handlers.impl.FollowingCompaniesHandler;
+
+import java.util.HashMap;
+
+public class FollowCompanyCommand extends Command{
+
+
+    public FollowCompanyCommand(HashMap<String, Object> args) {super(args);}
+
+    @Override
+    public Object execute() {
+
+        validateArgs(new String[]{"userId", "companyId"});
+
+        String userId = (String) args.get("userId");
+        String companyId = (String) args.get("companyId");
+        FollowingCompaniesHandler dbHandler = (FollowingCompaniesHandler) this.dbHandler;
+        dbHandler.followCompany(userId, companyId);
+
+        return null;
+    }
 }
--- src/main/java/com/linkedin/replica/connections/commands/impl/UnfollowCompanyCommand.java
+++ src/main/java/com/linkedin/replica/connections/commands/impl/UnfollowCompanyCommand.java
@@ -1,4 +1,22 @@
 package com.linkedin.replica.connections.commands.impl;
 
-public class UnfollowCompanyCommand {
+import com.linkedin.replica.connections.commands.Command;
+import com.linkedin.replica.connections.database.handlers.impl.FollowingCompaniesHandler;
+
+import java.util.HashMap;
+
+public class UnfollowCompanyCommand extends Command {
+
+    public UnfollowCompanyCommand(HashMap<String, Object> args) {super(args);}
+
+    @Override
+    public Object execute() {
+        validateArgs(new String[]{"userId", "companyId"});
+
+        String userId = (String) args.get("userId");
+        String companyId = (String) args.get("companyId");
+        FollowingCompaniesHandler dbHandler = (FollowingCompaniesHandler) this.dbHandler;
+        dbHandler.followCompany(userId, companyId);
+        return null;
+    }
 }
